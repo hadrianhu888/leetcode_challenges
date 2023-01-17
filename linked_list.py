5bd05d30dcdbddfb0c86b93b6a58414f4fcf8190
hadrianhu888 <hadrian.hu@gmail.com>
2023-01-17 17:40:43 -0500

    added iterative append left functionality

--- linked_list.py
+++ linked_list.py
@@ -30,7 +30,11 @@ class LinkedList:
         l1_length = l1.l_length()
         l2_length = l2.l_length()
         l1_sub_l2 = l1_length - l2_length
+        l2_sub_l1 = l2_length - l1_length
         if l1_sub_l2 > 0:
-            
-            
-        
+            for i in l1_sub_l2:
+                l2[i].add_left(['0'])
+        else:
+            for i in l2_sub_l1:
+                l1[i].add_left(['0'])
+
